6fb4d1644fbd57d70f889ace8f85145ec4236a3b
Jonathan Reichelt Gjertsen <jonath.re@gmail.com>
2017-11-26 18:04:03 +0100

    Make the example trigger make more sense

--- triggers/example.py
+++ triggers/example.py
@@ -1,5 +1,5 @@
-def trigger_example(**args):
-    if "do_it" in args and args["do_it"]:
+def trigger_example(do_it):
+    if do_it:
         return True, ("First value", "Second value", "Third value", )
     else:
         return False, (None,)
